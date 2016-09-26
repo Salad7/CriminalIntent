d7c4d0003e100bc2ad0ee0ac9a2be460bd663493
Mohamed Salad <msalad@uncc.edu>
2016-09-25 23:57:29 -0400

    Fragment capabilities added
    -Added CrimeFragment
    -Added fragment manager
    -wired edittext inside CrimeFragment

--- app/src/main/java/com/example/itsadmin/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/itsadmin/criminalintent/CrimeFragment.java
@@ -1,11 +1,52 @@
 package com.example.itsadmin.criminalintent;
 
-import android.app.Fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * Created by itsadmin on 9/25/2016.
  */
-public class CrimeFragment extends Fragment{
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+    private EditText mTextField;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState)
+    {
+        View v = inflater.inflate(R.layout.fragment_crime,parent,false);
+        mTextField = (EditText) v.findViewById(R.id.crime_title);
+        mTextField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        return v;
 
 
+    }
 }
